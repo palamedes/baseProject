7c5fa74ac2ac14867d9485f761679bd17ac4776a
Jason Ellis <palamedes@rocketmail.com>
2015-12-04 18:49:23 -0500

    Adding lots of devise stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,23 @@
 Rails.application.routes.draw do
 
-  get 'home/index'
-
-  devise_for :users, controllers: {
-                       registrations: :registrations,
-                       sessions: :sessions,
-                       confirmations: :confirmations,
-                       passwords: :passwords
-                   }
+  devise_for :users,
+             controllers: {
+                 registrations:       "users/registrations",
+                 sessions:            "users/sessions",
+                 confirmations:       "users/confirmations",
+                 passwords:           "users/passwords",
+                 unlocks:             "users/unlocks",
+                 omniauth_callbacks:  "users/omniauth_callbacks"
+             },
+             path_names: {
+                 sign_in:       "login",
+                 sign_out:      "logout",
+                 password:      "secret",
+                 confirmation:  "verification",
+                 unlock:        "unblock",
+                 registration:  "register",
+                 sign_up:       "signup"
+             }
 
   root "home#index"
 
