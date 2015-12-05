db29cdedc46b72df6f1f0775c8b418625aebc062
Jason Ellis <palamedes@rocketmail.com>
2015-12-05 10:05:15 -0500

    Make sure admin is admin

--- app/controllers/administration_controller.rb
+++ app/controllers/administration_controller.rb
@@ -1,6 +1,13 @@
 class AdministrationController < ApplicationController
-  protect_from_forgery with: :exception
+  before_action :ensure_user_is_admin
+
+  def index
+  end
 
   private
+
+  def ensure_user_is_admin
+    redirect_to root_path unless current_user && current_user.has_role? :admin
+  end
 
 end
